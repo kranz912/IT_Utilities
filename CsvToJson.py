5f24cb5546043c050e2ed9b4f8e3b9fc0c113b3f
kranz912 <me@kranznikolai.com>
2020-01-17 23:48:33 -0500

    added CsvToJson

---
+++ CsvToJson.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import argparse
+
+
+argparser = argparse.ArgumentParser(description="A utility to convert csv file to json file")
+argparser.add_argument('source',action='store', metavar='<source>' ,help='source csv file',)
+argparser.add_argument('destination',action='store',metavar='<destination>', help='destination json file')
+
+args = argparser.parse_args()
+print(args)
+try:
+    pass
+    csv = pd.read_csv(args.source)
+    csv.to_json(args.destination)
+except Exception as e:
+    print(e)
